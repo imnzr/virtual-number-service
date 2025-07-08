f3cb4242a07bcbb20eff3ccec498f07fce4c07a9
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-08 21:42:26 +0700

    add: routes user

---
+++ virtual-number-be/routes/user_routes/user_routes.go
@@ -0,0 +1,15 @@
+package userroutes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	usercontroller "github.com/imnzr/virtual-number-service/controller/user_controller"
+)
+
+// func UserRoutes(router *httprouter.Router, userController usercontroller.UserControllerInterface) {
+// 	router.POST("/api/v1/user/create", userController.CreateUser())
+// }
+
+func UserRoutes(router *fiber.App, userController usercontroller.UserControllerInterface) {
+	router.Post("/api/v1/user/create", userController.CreateUser)
+	router.Post("/api/v1/user/login", userController.LoginUser)
+}
