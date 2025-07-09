9afebcff5a930ae400935733677cf17ec48b91f2
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-09 20:49:44 +0700

    fix: bug

--- virtual-number-be/controller/user_controller/user_controller_implement.go
+++ virtual-number-be/controller/user_controller/user_controller_implement.go
@@ -7,6 +7,8 @@ import (
 	"github.com/imnzr/virtual-number-service/config"
 	"github.com/imnzr/virtual-number-service/models"
 	userservice "github.com/imnzr/virtual-number-service/service/user_service"
+	"github.com/imnzr/virtual-number-service/web/request"
+	"github.com/imnzr/virtual-number-service/web/response"
 )
 
 type UserControllerImplement struct {
@@ -45,7 +47,22 @@ func (uc *UserControllerImplement) ForgotPassword(controller *fiber.Ctx) error {
 
 // GetAllUsers implements UserControllerInterface.
 func (uc *UserControllerImplement) GetAllUsers(controller *fiber.Ctx) error {
-	panic("unimplemented")
+	users, err := uc.UserService.GetAllUsers(controller.Context())
+	if err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	var userResponse []response.UserResponse
+	for _, user := range users {
+		userResponse = append(userResponse, response.UserResponse{
+			Id:       user.Id,
+			Username: user.Username,
+			Email:    user.Email,
+		})
+	}
+
+	return controller.Status(200).JSON(userResponse)
 }
 
 // GetUserByEmail implements UserControllerInterface.
@@ -55,7 +72,29 @@ func (uc *UserControllerImplement) GetUserByEmail(controller *fiber.Ctx) error {
 
 // GetUserById implements UserControllerInterface.
 func (uc *UserControllerImplement) GetUserById(controller *fiber.Ctx) error {
-	panic("unimplemented")
+	paramsId := controller.Params("id")
+	userId, err := strconv.Atoi(paramsId)
+
+	if err != nil || userId <= 0 {
+		return controller.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	result, err := uc.UserService.GetUserById(controller.Context(), userId)
+	if err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	response := response.UserResponse{
+		Id:       result.Id,
+		Username: result.Username,
+		Email:    result.Email,
+	}
+
+	return controller.Status(200).JSON(response)
 }
 
 // LoginUser implements UserControllerInterface.
@@ -82,14 +121,23 @@ func (uc *UserControllerImplement) LoginUser(controller *fiber.Ctx) error {
 		})
 	}
 
-	return controller.JSON(fiber.Map{
-		"token": token,
-		"user": fiber.Map{
-			"Id":       user.Id,
-			"Username": user.Username,
-			"Email":    user.Email,
-		},
-	})
+	response := response.UserLoginResponse{
+		Id:       user.Id,
+		Username: user.Username,
+		Email:    user.Email,
+		Token:    token,
+	}
+
+	return controller.Status(200).JSON(response)
+
+	// return controller.JSON(fiber.Map{
+	// 	"token": token,
+	// 	"user": fiber.Map{
+	// 		"Id":       user.Id,
+	// 		"Username": user.Username,
+	// 		"Email":    user.Email,
+	// 	},
+	// })
 }
 
 // LogoutUser implements UserControllerInterface.
@@ -99,12 +147,82 @@ func (uc *UserControllerImplement) LogoutUser(controller *fiber.Ctx) error {
 
 // UpdateUserEmail implements UserControllerInterface.
 func (uc *UserControllerImplement) UpdateUserEmail(controller *fiber.Ctx) error {
-	panic("unimplemented")
+	var user models.User
+
+	idParams := controller.Params("id")
+	userId, err := strconv.Atoi(idParams)
+
+	if err != nil || userId <= 0 {
+		return controller.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if err := controller.BodyParser(&user); err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	getUser, err := uc.UserService.GetUserById(controller.Context(), userId)
+	if err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	emailUpdated, err := uc.UserService.UpdateUserEmail(controller.Context(), getUser.Id, user.Email)
+	if err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	response := response.UserResponse{
+		Id:       emailUpdated.Id,
+		Username: emailUpdated.Username,
+		Email:    emailUpdated.Email,
+	}
+
+	return controller.Status(200).JSON(response)
 }
 
 // UpdateUserPassword implements UserControllerInterface.
 func (uc *UserControllerImplement) UpdateUserPassword(controller *fiber.Ctx) error {
-	panic("unimplemented")
+	var request request.UpdatePasswordRequest
+
+	paramsId := controller.Params("id")
+	userId, err := strconv.Atoi(paramsId)
+
+	if err != nil || userId <= 0 {
+		return controller.Status(400).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if err := controller.BodyParser(&request); err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// GET USER ID
+	getUser, err := uc.UserService.GetUserById(controller.Context(), userId)
+	if err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// UPDATE PASSWORD
+	updatePasswordUser, err := uc.UserService.UpdateUserPassword(controller.Context(), getUser.Id, request)
+	if err != nil {
+		return controller.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return controller.Status(200).JSON(updatePasswordUser)
 }
 
 // UpdateUserUsername implements UserControllerInterface.
