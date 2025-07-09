c9d66996b7dc4dcbdb3eaf801ce2456f6fe3ad0e
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-09 20:49:07 +0700

    add: update email, update password, get all users, and get user by id

--- virtual-number-be/routes/user_routes/user_routes.go
+++ virtual-number-be/routes/user_routes/user_routes.go
@@ -5,13 +5,14 @@ import (
 	usercontroller "github.com/imnzr/virtual-number-service/controller/user_controller"
 )
 
-// func UserRoutes(router *httprouter.Router, userController usercontroller.UserControllerInterface) {
-// 	router.POST("/api/v1/user/create", userController.CreateUser())
-// }
-
 func UserRoutes(router *fiber.App, userController usercontroller.UserControllerInterface) {
 	router.Post("/api/v1/user/create", userController.CreateUser)
 	router.Post("/api/v1/user/login", userController.LoginUser)
 
 	router.Put("/api/v1/user/update-username/:id", userController.UpdateUserUsername)
+	router.Put("/api/v1/user/update-email/:id", userController.UpdateUserEmail)
+	router.Put("/api/v1/user/update-password/:id", userController.UpdateUserPassword)
+
+	router.Get("/api/v1/user/:id", userController.GetUserById)
+	router.Get("/api/v1/users", userController.GetAllUsers)
 }
