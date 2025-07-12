3452831a322832af49cbad21246f2ce06b78bb7f
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-12 20:41:40 +0700

    create: declare country routes to main.go

--- virtual-number-be/main.go
+++ virtual-number-be/main.go
@@ -6,10 +6,13 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/imnzr/virtual-number-service/config"
+	countrycontroller "github.com/imnzr/virtual-number-service/controller/country_controller"
 	usercontroller "github.com/imnzr/virtual-number-service/controller/user_controller"
 	"github.com/imnzr/virtual-number-service/database"
 	userrepository "github.com/imnzr/virtual-number-service/repository/user_repository"
+	countryroutes "github.com/imnzr/virtual-number-service/routes/country_routes"
 	userroutes "github.com/imnzr/virtual-number-service/routes/user_routes"
+	countryservice "github.com/imnzr/virtual-number-service/service/country_service"
 	userservice "github.com/imnzr/virtual-number-service/service/user_service"
 )
 
@@ -36,11 +39,15 @@ func main() {
 	userservice := userservice.NewUserService(userrepository, db)
 	usercontroller := usercontroller.NewUserController(userservice)
 
+	countryService := countryservice.NewCountryService()
+	countryController := countrycontroller.NewCountryController(countryService)
+
 	// Initialize the router and define routes
 	router := fiber.New()
 
 	// Define user routes
 	userroutes.UserRoutes(router, usercontroller)
+	countryroutes.CountryRoutes(router, countryController)
 
 	// Server listening configuration
 	router.Listen("localhost:8080")
